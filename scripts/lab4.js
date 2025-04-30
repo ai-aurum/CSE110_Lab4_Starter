d700aa75aaa20ba71482e506f7c0432a76feb9ec
Aldrin Ilagan <ailagan@ucsd.edu>
2025-04-30 01:35:53 -0700

    Fixed test errors

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add) {
+    if (add && (typeof num1 == typeof num2 ==  number)) {
         let result = 0;
 
         result = num1 + num2;
@@ -25,6 +25,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (prices.length == 0 || (typeof discount != number) || (discount < 0 || discount > 1)) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
