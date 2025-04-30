97c0f3b4b6df5ed1a82db48768c3c0f2d9c03958
Aldrin Ilagan <ailagan@ucsd.edu>
2025-04-30 01:46:36 -0700

    Fixed logic of edge casees

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,9 +6,8 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add == true && (typeof num1 == typeof num2 ==  "number")) {
+    if (add == true && (typeof num1 == typeof num2 && typeof num1 == "number")) {
         let result = 0;
-
         result = num1 + num2;
 
         return result;
@@ -25,7 +24,7 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if (Array.isArray(prices) || 
+    if (!Array.isArray(prices) || 
        (prices.length == 0) ||
        (typeof discount != "number") || 
        (discount < 0 || discount > 1)) {
@@ -41,6 +40,6 @@ function discountPrices(prices, discount) {
 
     return discounted;
 }
-console.log(sumValues(1, "halo", true));
-console.log(discountPrices([], 13));
+console.log(sumValues(1, 2, true));
+console.log(discountPrices([1], 0.5));
 module.exports = {sumValues, discountPrices};
